dc9ff57dc73bbf5c6c96ffe1e43e299ec8281ca8
DrToil <axmed13@hotmail.com>
2018-09-27 00:04:53 +0100

    javascript DOM Manipulation

---
+++ udemy/the web developer bootcamp/11 Section 13 (JS DOM)/Important Selector Methods/Selector Exercise/selector.js
@@ -0,0 +1,11 @@
+/*alert('Js is connected.');*/
+
+document.getElementById("first");
+document.querySelector("#first");
+document.getElementsByClassName("special");
+document.getElementsByClassName("special")[0];
+document.querySelector(".special");
+document.querySelectorAll('.special')[0];
+document.getElementsByTagName("p")[0]; 
+document.querySelector("p");
+document.querySelectorAll("p")[0];
---
+++ udemy/the web developer bootcamp/11 Section 13 (JS DOM)/Manipulating/manipulating.js
@@ -0,0 +1,5 @@
+var h1 = document.querySelector("h1");
+
+h1.style.color = "blue";
+h1.align = "center";
+h1.style.border = "4px solid red";
